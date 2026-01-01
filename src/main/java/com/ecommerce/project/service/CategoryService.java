2cbc2e5f0264b2649f9ddbb4c58d1bf035b159b8
Pranaav <pranaavarunbabu@gmail.com>
2026-01-01 17:33:05 +0530

    Sorting now added

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 //Interface to promote loose coupling
 public interface CategoryService {
-    CategoryResponse getALlCategories(Integer pageNumber , Integer pageSize);
+    CategoryResponse getALlCategories(Integer pageNumber , Integer pageSize , String sortBy,  String sortOrder);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 
     CategoryDTO deleteCategory(Long categoryId);
